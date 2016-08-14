d05ed00bf31c18881f2c49c2831750e2c42b8218
kgilds <kevingilds@gmail.com>
2016-08-13 22:47:28 -0400

    inital build for getReal package of functions

---
+++ R/hr_achieved.R.R
@@ -0,0 +1,27 @@
+# ' Percent of students achieving healthy relationship outcome
+
+
+# `Determine the percent of students achieving the healthy releationship outcome
+#
+#' @param Write in pre or post for time the survey was taken
+#' hr_achieved()
+
+hr_achieved <- function(Time) {
+    
+        if(Time =="Pre"){
+            dat <-filter(hr, Time =="Pre")
+            
+        } else if(Time == "Post"){
+            dat <- filter(hr, Year_Id =="Post")
+        
+        }
+        
+        
+        high <- filter(dat, Post >=4.45)
+        hr_temp_1 <- nrow(high)/nrow(dat)
+        hr_temp_1 <- round(hr_temp_1*100,2)
+        hr_temp_1 <- paste0(hr_temp_1,"%", sep="")
+        pander(hr_temp_1)
+        
+    }   
+ 
